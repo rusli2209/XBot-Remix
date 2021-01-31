aee4e1b77d0e65c8bd921caa5be8d6786e29e5f8
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-01-31 22:04:08 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -8,44 +8,70 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^P(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
-    sleep(2)
-    await typew.edit("`Assalamualaikum.....`")
-# Owner @Si_Dian
-
-
 @register(outgoing=True, pattern='^p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit(f"**Hallo Semua Saya {DEFAULTUSER}**")
+    await typew.edit(f"**Misi lort, ikut nimbrung**")
     sleep(2)
-    await typew.edit("`Assalamualaikum.....`")
-# Owner @Si_Dian
+    await typew.edit("**Assalamualaikum...**")
+# Owner @eruuu
 
-
-@register(outgoing=True, pattern='^L(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
-    sleep(1)
-    await typew.edit("`Waallaikumsalam......`")
-# Owner @Si_Dian
 
 
 @register(outgoing=True, pattern='^l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Astaghfirulloh Jawab Salam Dong...`")
+    await typew.edit("**Jawab Salam Dong...**")
     sleep(1)
-    await typew.edit("`Waallaikumsalam.....`")
-# Owner @Si_Dian
+    await typew.edit("**Wa'alaikumsalam...**")
+# Owner @erruuu
+
+
+
+@register(outgoing=True, pattern='^ll(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("*Jawab Salam Dong...*")
+    sleep(1)
+    await typew.edit("**Wa'alaikumsalam lort**")
+# Owner @erruuu
+
+
+
+@register(outgoing=True, pattern='^arto(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("*protecc ah*")
+    sleep(1)
+    await typew.edit("/protecc artoria")
+# Owner @erruuu
+
+
+
+@register(outgoing=True, pattern='^Arthur(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("*protecc ah*")
+    sleep(1)
+    await typew.edit("/protecc Arthur")
+# Owner @erruuu
+
+
+
+@register(outgoing=True, pattern='^heroine(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("*protecc ah*")
+    sleep(1)
+    await typew.edit("/protecc mysterious heroine x xx")
+# Owner @erruuu
+
 
 
 CMD_HELP.update({
