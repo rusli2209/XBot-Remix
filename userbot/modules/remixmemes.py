1366590bf79407996e707df16c5823dbb0539f5e
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-01-31 15:13:14 +0700

    Update remixmemes.py

--- userbot/modules/remixmemes.py
+++ userbot/modules/remixmemes.py
@@ -16,6 +16,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
+@bot.on(admin_cmd(pattern=r"boxs$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"boxs$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "`boxs...`")
+    deq = deque(list("🟥🟧🟨🟩🟦🟪🟫⬛⬜"))
+    for _ in range(999):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
 @bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
