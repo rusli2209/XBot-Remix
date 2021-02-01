7ac043b3d5cf4502857b37fd294d2f87e2411779
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-02 05:11:04 +0700

    Update myalive.py

--- userbot/modules/myalive.py
+++ userbot/modules/myalive.py
@@ -2,28 +2,15 @@ from time import sleep
 from userbot.events import register
 
 
-@register(pattern='^.boxs(?: |$)(.*)', outgoing=True)
-async def _(event):
-    if event.fwd_from:
-        return
-    event = await edit_or_reply(event, "`boxs...`")
-    deq = deque(list("🟥🟧🟨🟩🟦🟪🟫⬛⬜"))
-    for _ in range(999):
-        await asyncio.sleep(0.3)
-        await event.edit("".join(deq))
-        deq.rotate(1)
-# Create by myself @localheart
-
-
 @register(outgoing=True, pattern='^.akmal(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Akmal`")
-    sleep(3)
-    await typew.edit("`Cowok`")
     sleep(1)
-    await typew.edit("`Tinggal Disulsel, Salam Kenal:)`")
+    await typew.edit("`Hai Akmal`")
+    sleep(1)
+    await typew.edit("`ohayou`")
+    sleep(1)
+    await typew.edit("`Salam Kenal:)`")
 # Create by myself @localheart
 
 
