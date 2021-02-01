d21f5e9a287d0af87b61a65e200953ed4f8ae6b0
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-02 06:39:33 +0700

    Update canda.py

--- userbot/modules/canda.py
+++ userbot/modules/canda.py
@@ -11,11 +11,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.bagi(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Woii...**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Bagi harem nya dong**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Punya gw masih dikit hiks**")
 # Owner @erruuu
 
@@ -23,87 +23,43 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.sad(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Aku cinta dia**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Walau dia tidak cinta aku**")
-    sleep(2)
-    await typew.edit("**Karena cinta tidak harus memiliki.**")
-    sleep(2)
-    await typew.edit("**Hiksss...**")
-# Owner @erruuu
-
-
-@register(outgoing=True, pattern='^.beban(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit(f"**Dari 7,4 miliar**")
-    sleep(2)
-    await typew.edit("**sampah di bumi**")
-    sleep(2)
-    await typew.edit("**eh maksudnya manusia di bumi**")
-    sleep(2)
-    await typew.edit("**hanya kamu yang menjadi beban**")
-    sleep(2)
-    await typew.edit("**di kehidupan alam ini**")
-    sleep(2)
-    await typew.edit("**canda wkwk, jangan baper...**")
+    await typew.edit("**Karena cinta tidak harus memiliki.**")
+    sleep(0.5)
+    await typew.edit("**Hiksss...**")
 # Owner @erruuu
 
 
 @register(outgoing=True, pattern='^.newmem(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Hallo**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Selamat bergabung!**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Salken yaa**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Semoga betah disini**")
-# Owner @erruuu
-
-
-@register(outgoing=True, pattern='^.malam(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit(f"**Konbanwa**")
-    sleep(2)
-    await typew.edit("**Selamat malam semuanya**")
-    sleep(2)
-    await typew.edit("**Tidur-tidur**")
-    sleep(2)
-    await typew.edit("**Jaga kesehatan lu**")
-    sleep(2)
-    await typew.edit("**Soalnya kalo sakit ga ada yg peduli**")
-    sleep(2)
-    await typew.edit("**Sad...**")
-    sleep(2)
-    await typew.edit("**Canda kak hehe**")
-    sleep(2)
-    await typew.edit("**Ada aku yang peduli kok...**")
-    sleep(2)
-    await typew.edit("**Tapi boong**")
-    sleep(2)
-    await typew.edit("**Xixixii...**")
 # Owner @erruuu
 
 
 @register(outgoing=True, pattern='^.ohayou(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Ohayou**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Sekai**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Good Morning**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**World**")
-    sleep(2)
+    sleep(0.5)
     await typew.edit("**Pagii**")
 # Owner @erruuu
 
@@ -111,68 +67,59 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.g(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Woii...**")
-    sleep(2)
-    await typew.edit("`Ganteng doang`")
-    sleep (2)
-    await typew.edit("`Ganteng doang jemput cewe depan gang`")
+    sleep(0.5)
+    await typew.edit("Ganteng doang")
+    sleep (0.5)
+    await typew.edit("jemput cewe depan gang")
 # Owner @Si_Dian
 
 
 @register(outgoing=True, pattern='^.c(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("`Cantik doang`")
-    sleep(1)
-    await typew.edit("`Tapi chat cowok lebih dari satu`")
+    sleep(0.5)
+    await typew.edit("Cantik doang")
+    sleep(0.5)
+    await typew.edit("Tapi chat cowok lebih dari satu")
 # Owner @Si_Dian
 
-
-@register(outgoing=True, pattern='^.c(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1)
-    await typew.edit("`Cantik doang`")
-    sleep(1)
-    await typew.edit("`Tapi chat cowok lebih dari satu`")
-# Owner @Si_Dian
 
 @register(outgoing=True, pattern='^.brokn(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(2)
+    sleep(1)
     await typew.edit(f"**I'M A BROKENT HOME.**")
-    sleep(5)
+    sleep(2)
     await typew.edit("`Tangisan hanya mengacaukan segalanya tapi senyuman membuat mereka yakin aku Tegar`")
-    sleep(5)
+    sleep(2)
     await typew.edit("`Setiap anak ingin keluarga yang sempurna\ntapi tidak semua anak memilikinya`")
-    sleep(5)
+    sleep(2)
     await typew.edit("`Sayangilah kedua orang tuamu dengan\nsepenuh hati selagi masih ada🙂`")
 
 
 @register(outgoing=True, pattern='^.oe(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**oee....**")
-    sleep(3)
+    sleep(1)
     await typew.edit("`Muka kalian 8 bit...`")
-    sleep(3)
+    sleep(1)
     await typew.edit("`Kayak Monyet`")
-    sleep(3)
+    sleep(1)
     await typew.edit("`Muka gw Gak Burik Kek Kalian`")
 
 
 @register(outgoing=True, pattern='^.tidr(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(1)
+    sleep(0.5)
     await typew.edit(f"**Eh, beban keluarga tdr woi**")
-    sleep(2)
+    sleep(1)
     await typew.edit("`Sadar gadangnya bukan untukmu.`")
-    sleep(2)
+    sleep(1)
     await typew.edit("`Melainkan untuk dia di akun satu`")
 
 
@@ -182,7 +129,7 @@ async def typewriter(typew):
     sleep(1)
     await typew.edit(f"**Eh, yelah pc doang jadian kagak**")
     sleep(2)
-    await typew.edit("`Percuma Jadian putus iya, ngewe kgk.`")
+    await typew.edit("`Percuma Jadian, putus iya, ngewe kgk.`")
     sleep(2)
     await typew.edit("`Canda ngewe:v`")
     sleep(2)
