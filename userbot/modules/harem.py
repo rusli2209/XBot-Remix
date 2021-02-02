4016ca681de15b67ef522bbb5c993a62ae71b602
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-02 07:07:51 +0700

    Update harem.py

--- userbot/modules/harem.py
+++ userbot/modules/harem.py
@@ -12,7 +12,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(0.000001)
-    await typew.edit("/protecc artoria yi medb Nightingale (Santa) NP1 2. Yu Miaoyi NP1 3. Katsushika Hokusai (Saber) NP2 4. Carmilla NP1 5. Mysterious Alter-Ego Lambda NP1 6. Tamamo-no-Mae NP3 7. Chacha NP2 8. Circe NP2 9. Medea (Lily) NP2 10. Murasaki Shikibu (Rider) NP2 11. Sessyoin Kiara (Moon Cancer) NP2 12. Space Ishtar NP1 13. Bradamante NP1 14. Brynhildr (Berserker) NP2 15. Tomoe Gozen (Saber) NP2 16. Irisviel (Holy Grail) NP1 17. Artoria Pendragon (Archer) NP1 18. Katou Danzo NP1 19. Stheno NP2 20. Nero Claudius (Bride) NP2")
+    await typew.edit("/protecc artoria shiki yi medb Nightingale yu ke i ii iii Hokusai Carmilla Mysterious Mae chacha circe medea murasaki kiara ishtar bradamante brynhildr tomoe gozen irisviel von katou stheno euryale nero enkidu yang")
 # Owner @erruuu
 
 
@@ -21,7 +21,7 @@ async def typewriter(typew):
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(0.000001)
-    await typew.edit("/protecc arthur Gilgamesh Mephistopheles Lancelot Siegfried Yagyu mac Cumhaill Henry Shi i Darius solomon Eric Charles Asclepius Mozart Angra David de")
+    await typew.edit("/protecc arthur gilgamesh edward eric ivan yu yi mephistopheles lancelot Siegfried yagyu mac cumhaill Henry Shi i ii iii Darius solomon Eric Charles Asclepius Mozart Angra David de")
 # Owner @erruuu
 
 
