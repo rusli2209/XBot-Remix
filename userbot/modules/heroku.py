941a237a43f020fa9a3debcfc7cb48ac768b4cbd
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-03 07:43:54 +0700

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -124,7 +124,7 @@ async def set_var(var):
 """
 
 
-@register(outgoing=True, pattern=r"^.usage(?: |$)")
+@register(outgoing=True, pattern=r"^.dyno(?: |$)")
 async def dyno_usage(dyno):
     """
         Get your account Dyno Usage
@@ -187,6 +187,76 @@ async def dyno_usage(dyno):
                 "-> `Sisa Dyno Bulan Ini`:\n"
                 f"    •**{hours} jam - {minutes} menit  "
                 f"-  {percentage}%**\n"
+                "╰━━━━━━━━━━━━━━━━━━━━╯"
+            )
+            await asyncio.sleep(20)
+            await dyno.delete()
+            return True
+
+
+@register(outgoing=True, pattern=r"^.usage(?: |$)")
+async def dyno_usage(dyno):
+    """
+        Get your account Dyno Usage
+    """
+    await dyno.edit("`Getting Information...`")
+    useragent = (
+        'Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/81.0.4044.117 Mobile Safari/537.36'
+    )
+    user_id = Heroku.account().id
+    headers = {
+        'User-Agent': useragent,
+        'Authorization': f'Bearer {HEROKU_API_KEY}',
+        'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
+    }
+    path = "/accounts/" + user_id + "/actions/get-quota"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(heroku_api + path, headers=headers) as r:
+            if r.status != 200:
+                await dyno.client.send_message(
+                    dyno.chat_id,
+                    f"`{r.reason}`",
+                    reply_to=dyno.id
+                )
+                await dyno.edit("`Can't get information...`")
+                return False
+            result = await r.json()
+            quota = result['account_quota']
+            quota_used = result['quota_used']
+
+            """ - User Quota Limit and Used - """
+            remaining_quota = quota - quota_used
+            percentage = math.floor(remaining_quota / quota * 100)
+            minutes_remaining = remaining_quota / 60
+            hours = math.floor(minutes_remaining / 60)
+            minutes = math.floor(minutes_remaining % 60)
+
+            """ - User App Used Quota - """
+            Apps = result['apps']
+            for apps in Apps:
+                if apps.get('app_uuid') == app.id:
+                    AppQuotaUsed = apps.get('quota_used') / 60
+                    AppPercentage = math.floor(
+                        apps.get('quota_used') * 100 / quota)
+                    break
+            else:
+                AppQuotaUsed = 0
+                AppPercentage = 0
+
+            AppHours = math.floor(AppQuotaUsed / 60)
+            AppMinutes = math.floor(AppQuotaUsed % 60)
+
+            await dyno.edit(
+                "**Dyno Usage**:\n\n╭━━━━━━━━━━━━━━━━━━━━╮\n"
+                f"-> `Penggunaan Dyno `  **Bot Ini**:\n"
+                f"    •**{AppHours} jam - "
+                f"{AppMinutes} menit - {AppSeconds} detik**"
+                "\n ◐━─━─━─━─━──━─━─━─━─━◐\n"
+                "-> `Sisa Dyno Bulan Ini`:\n"
+                f"    •**5{hours} jam - {minutes} menit  "
+                f"-  ?%**\n"
                 "╰━━━━━━━━━━━━━━━━━━━━╯"
             )
             await asyncio.sleep(20)
