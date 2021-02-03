6ee79a8a5ee9444d695a916c2dc4481aed7dd4cd
rusli2209 <64939964+rusli2209@users.noreply.github.com>
2021-02-04 06:25:06 +0700

    Update harem.py

--- userbot/modules/harem.py
+++ userbot/modules/harem.py
@@ -11,33 +11,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^s(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(0.000001)
+    sleep(0)
     await typew.edit("/protecc quetzalcoatl jinako chevalier artoria qin rhyme matahari shiki yi medb osakabehime kiyohime Nightingale yu ke i ii iii Hokusai Carmilla mata Mysterious Mae drake europa paris boudica chacha circe medea murasaki kiara ishtar williams jeanne jane bradamante brynhildr tomoe gozen irisviel von katou medusa parvati orion opera stheno euryale nero enkidu hassan yang abigail")
 # Owner @erruuu
-
-
-
-@register(outgoing=True, pattern='^h(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(0.000001)
-    await typew.edit("/protecc arthur gilgamesh edward eric arjuna emiya gaius goetia ivan yu yi mephistopheles benkei chiron lancelot the Siegfried sieg spartacus drake romulus sakata antonio helmes red yagyu mac cumhaill Henry henri roberts paris columbus bedivere amakusa okada hans prince iskandar tristan Shi i ii iii william geronimo hood georgios hijikata caligula alexander li lu lobo aesclepius Darius solomon Eric hassan chen ashwatthama jason james Charles enkidu asterios Asclepius yan cu merlin  mozart Angra David de heracles karna fuuma tawara arash ozymandias odysseus opera orion")
-# Owner @erruuu
-
 
 
 @register(outgoing=True, pattern='^xx(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(0.000001)
+    sleep(0)
     await typew.edit("/protecc mysterious heroine x xx")
 # Owner @erruuu
 
 
-@register(outgoing=True, pattern='^ll(?: |$)(.*)')
+@register(outgoing=True, pattern='^h(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(0.000001)
+    sleep(0)
     await typew.edit("/protecc hikari hikari oono kirigaya mio atago gaen araragi chisa yuuna izumi izuna nao ruka rikka erice erika chihiro nakano uesugi yui yukino hanyuu medusa mito mii boudica akari tomoe mami papi kagome beatrice eru kyons sister sae cabashira ichinose rias kanao akeno futaba koneko meteora aoko kyouko shinku youmu ryouko asuna haruhi natsumi lelei margaret")
 # Owner @erruuu
 
@@ -47,8 +37,6 @@ CMD_HELP.update({
     "`s`\
 \nUsage: Protecc servant (beta).\
 \n\n`h`\
-\nUsage: Protecc husbu (beta).\
-\n\n`ll`\
 \nUsage: Protecc loli (beta).\
 \n\n`xx`\
 \nUsage: Protecc servant char mysterious."
